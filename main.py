21538b99ab2f03dc32ddc0d1d0dbdad5c0f5af0d
Brian McDowell <brianmcd08@gmail.com>
2026-04-15 16:27:02 -0400

    final commit

--- main.py
+++ main.py
@@ -1,11 +1,53 @@
 from dotenv import load_dotenv
+from langchain.messages import AIMessage, HumanMessage
+from langgraph.graph import END, MessagesState, StateGraph
+
+from nodes import run_agent_reasoning, tool_node
 
 load_dotenv()
 
 
-def main():
-    print("Hello from langgraph-react-agent!")
+def should_continue(state: MessagesState) -> str:
+    # check last message
+    last_message = state["messages"][LAST]
+    if not isinstance(last_message, AIMessage) or not last_message.tool_calls:
+        return END
+    return ACT
+
+
+AGENT_REASON = "agent_reason"
+ACT = "act"
+LAST = -1
+
+flow = StateGraph(MessagesState)
+flow.add_node(AGENT_REASON, run_agent_reasoning)
+flow.set_entry_point(AGENT_REASON)
+flow.add_node(ACT, tool_node)
+
+flow.add_conditional_edges(
+    AGENT_REASON,
+    should_continue,
+    {
+        END: END,
+        ACT: ACT,
+    },
+)
+
+flow.add_edge(ACT, AGENT_REASON)
+app = flow.compile()
+app.get_graph().draw_mermaid_png(output_file_path="flow.png")
 
 
 if __name__ == "__main__":
-    main()
+    print("ReAct LangGraph with Function Calling")
+    res = app.invoke(
+        {
+            "messages": [
+                HumanMessage(
+                    content="What is the weather in Tokyo? List it and then triple it."
+                )
+            ]
+        }
+    )
+
+    print(res["messages"][LAST].content)
